15c280a07468d924994f560359d2c6dbf3e1aac3
bdtoole <briantoole@gmail.com>
2017-01-14 20:25:22 -0800

    Finished base version functionality.  In addition, I decided to have just a single function that did all of the work instead of multiples for a few reasons.
    First, Powershell is a bit nonstandard with its variable scoping.
    Second, none of the earlier sub-functions would ever be called individually, so I didn't want them just floating out there.
    Third, this way reduces the number of variables being used and makes it a bit more clean - no more cases of multiple variables for a single piece of data.

--- RegexGenerator.ps1
+++ RegexGenerator.ps1
@@ -1,115 +1,83 @@
 <#
  # Reference: http://regexhero.net/reference/
+ # Testing: https://regex101.com
  #>
 
-#$inputStrings = ("T3st", "with", "varying", "lengths")
+#$inputStrings = ("T3sT", "with", "varying", "lengths")
 $inputStrings = ("T3sT", "w1tH", "d1fFerent", "w0rDs")
 
 Function Build-Regex
 {
-    $regexList = New-Object System.Collections.Generic.List[string]
-
-    [System.Collections.Generic.List[string]]$characterSets = Get-Character-Sets $inputStrings
-    
-    $optional = $False
-
-    ForEach ($i in 0..($characterSets.Count-1))
-    {
-        [string]$regex = Get-Regex-For-Character-Set $characterSets[$i]
-
-        If ($i -gt 0 -and $characterSets[$i].Length -lt $characterSets[$i-1].Length)
-        {
-            $optional = $True
-        }
-
-        If ($optional) { $regex += '?' }
-
-        $regexList.Add($regex)
-    }
-
-    Consolidate-Regex $regexList
-}
-
-Function Get-Character-Sets
-{
-    Param(
-    [System.Collections.Generic.List[string]]$stringList
+    $expressionsToTest = @(
+        '^\w*$'
+        ,'^[A-Za-z]*$'
+        ,'^[A-Z]*$'
+        ,'^[a-z]*$'
+        ,'^\d*$'
     )
 
+    $regexList = New-Object System.Collections.Generic.List[string]
     $characterSets = New-Object System.Collections.Generic.List[string]
+    $repetitionList = New-Object System.Collections.Generic.List[string]
+    $numOccurrences = 1
 
-    $longestLength = ($stringList | Measure-Object -Maximum -Property Length).Maximum
+    $longestLength = ($inputStrings | Measure-Object -Maximum -Property Length).Maximum
 
     ForEach ($i in 0..($longestLength-1))
     {
         [string]$charSet = ""
-        ForEach ($word in $stringList)
+        ForEach ($word in $inputStrings)
         {
             $charSet += $word[$i]
         }
         $characterSets.Add($charSet)
     }
 
-    return $characterSets
-}
-
-Function Get-Regex-For-Character-Set
-{
-    Param(
-    [string]$set
-    )
-
-    [string]$return = ""
-    [bool]$valid = $TRUE
-
-    $expressionsToTest = @(
-        '^\w*$'
-       ,'^[A-Za-z]*$'
-       ,'^[A-Z]*$'
-       ,'^[a-z]*$'
-       ,'^\d*$'
-    )
-
-    ForEach ($expression in $expressionsToTest)
+    ForEach ($i in 0..($characterSets.Count-1))
     {
-        $valid = ($set -cmatch $expression)
-        If ($valid) { $return = $expression }
-        Else { $return = $return }
-    }
-    Return ($return -split '[\^\*]')[1]
-}
+        [string]$regex = ""
+        [bool]$valid = $TRUE
 
-Function Consolidate-Regex
-{
-    Param(
-    [System.Collections.Generic.List[string]]$regexList
-    )
-
-    $consolidatedRegexList = $regexList
-
-    Write-Host $regexList
-
-    ForEach ($i in ($regexList.Count-1)..0)
-    {
-    
-        Write-Host $i $regexList[$i] $regexList[$i-1]
-        If ($i -gt 0 -and $regexList[$i] -eq $regexList[$i-1])
+        ForEach ($expression in $expressionsToTest)
         {
-            If ($regexList[$i] -ceq $regexList[$i-1])
-            {
-                $consolidatedRegexList[$i-1] = $consolidatedRegexList[$i-1]+"*"
-                $consolidatedRegexList.RemoveAt($i)
-            }
-            ElseIf ($regexList[$i] -eq "[A-Z]")
-            {
-                $consolidatedRegexList[$i-1] = "[A-Za-z]*"
-                $consolidatedRegexList.RemoveAt($i)
-            }
+            $valid = ($characterSets[$i] -cmatch $expression)
+            If ($valid) { $regex = $expression }
+            Else { $regex = $regex }
+        }
+        $regex = (($regex -split '[\^\*]')[1])
+        $regexList.Add($regex)
+    }
+
+    ForEach ($i in 0..($regexList.Count-2))
+    {
+        If ( $regexList[$i] -ceq $regexList[$i+1] ) 
+        {   
+            $numOccurrences++
+            $repetitionList.Add($numOccurrences)
+        }
+        Else
+        {
+            $numOccurrences = 1
+            $repetitionList.Add(0)
         }
     }
 
-    Write-Host $consolidatedRegexList
-    
+    ForEach ($i in ($regexList.Count-1)..0)
+    {
+        If ($repetitionList[$i] -gt 1) 
+        { 
+            If ($repetitionList[$i] -lt $repetitionList[$i+1]) { $repetitionList.RemoveAt($i) }
+            $regexList.RemoveAt($i+1)
+        }
+    }
+
+    ForEach ($i in ($consolidatedRegexList.Count-1))
+    {
+        If ($repetitionList[$i] -gt 0) { $regexList[$i] += "{1,$($repetitionList[$i]))}" }
+    }
+
+    Write-Host ([string]::Join("",$regexList))
+
 }
 
 Build-Regex
